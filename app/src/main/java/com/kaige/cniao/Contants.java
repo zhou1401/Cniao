9e8af9b5c3bfe7452fd3169a67dfdb9648797209
zhou <1006654899@qq.com>
2017-07-30 18:04:46 +0800

    主页界面展示商品从网络上获取

---
+++ app/src/main/java/com/kaige/cniao/Contants.java
@@ -0,0 +1,13 @@
+package com.kaige.cniao;
+
+/**
+ * Created by Administrator on 2017/7/30.
+ */
+
+public class Contants {
+    public static class API{
+        public static final String BASE_URL="http://112.124.22.238:8081/course_api/";
+        public static final String CAMPAIGN_HOME=BASE_URL + "campaign/recommend";
+    }
+}
+
---
+++ app/src/main/java/com/kaige/cniao/bean/Campaign.java
@@ -0,0 +1,43 @@
+package com.kaige.cniao.bean;
+
+/**
+ * Created by Administrator on 2017/7/30.
+ */
+import java.io.Serializable;
+
+
+public class Campaign implements Serializable {
+
+
+    private Long id;
+    private String title;
+
+    private String imgUrl;
+    private int i;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
+
